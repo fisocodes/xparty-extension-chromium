315d9e0c4cd887d958008160ff213b94ff32ded4
Oscar Figueroa <fisocodes@gmail.com>
2023-11-12 15:26:37 -0800

    Exploring API capabilities

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,1 +1,32 @@
-console.log('HELLO EXTENSION')
+console.log('XPARTY RUNNING')
+
+chrome.history.onVisited.addListener(function ({url: rawUrl})
+{
+    if(rawUrl)
+    {
+        const url = new URL(rawUrl)
+
+        if(url.hostname === 'www.youtube.com' && url.pathname === '/')
+        {
+            console.log('IN HOME')
+        }
+        else if(url.hostname === 'www.youtube.com' && url.pathname === '/watch' && url.searchParams.size === 2)
+        {
+            console.log('WATCHING A VIDEO')
+        }
+
+    }
+})
+
+chrome.runtime.onMessage.addListener(function(request)
+{
+    switch(request.type)
+    {
+        case 'video':
+            console.log('MESSAGE FROM VIDEO')
+            break
+        case 'pop-up':
+            console.log('MESSAGE FROM POP UP')
+            break
+    }
+})
