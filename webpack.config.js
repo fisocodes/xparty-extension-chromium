076ff65f70cc6038204856f96d2c1e070564bb1b
Oscar Figueroa <fisocodes@gmail.com>
2023-11-12 13:06:02 -0800

    Added Typescript and Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('node:path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports =  {
+    mode: 'production',
+    entry: {
+        serviceWorker: {
+            import: './src/service-worker/index.ts',
+            filename: 'service-worker.js'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'manifest.json'),
+                    to: path.resolve(__dirname, 'dist', 'manifest.json')
+                }
+            ]
+        })
+    ]
+}
